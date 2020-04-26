bcdba1cb4dba3afd787ef2f6bbc366cc994ba901
Idkirsch <s195483@student.dtu.dk>
2020-04-26 13:57:41 +0200

    array er initialiseret til 0'er

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ void evalTest();
 int main() {
     printf("Hello, World!\n");
 
-    char output[100];
+    char output[100] = {0};
     char eksempel[] = "hejmaltehej";
     evalTest(eksempel, output);
 
@@ -24,7 +24,7 @@ void evalTest(char * input, char * output){
     for (int i =0; i<11;i++){
         output[i] = input[i] - 32;
     }
-    output[11]='\0';
+    //output[11]='\0';
 }
 
 
