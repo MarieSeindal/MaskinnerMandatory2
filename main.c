02f17347f825dfe2ab742adb7fa10d65d5e834a6
peterrevsbech <peter.revsbech@gmail.com>
2020-04-26 13:32:38 +0200

    testprogram

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <malloc.h>
+
+void evalTest();
 
 int main() {
     printf("Hello, World!\n");
+
+    char output[100];
+    char eksempel[] = "hejmaltehej";
+    evalTest(eksempel, output);
+
+
+
+    printf("%s",output);
     return 0;
+}
+
+
+void evalTest(char * input, char * output){
+    for (int i =0; i<11;i++){
+        output[i] = input[i] - 32;
+    }
+    output[11]='\0';
 }
