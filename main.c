90a63a4f05f7bb1c9b39e74a08b28012f4c178e2
Marie Seindal <marie@seindal.dk>
2020-04-26 12:01:44 +0200

    ny branch

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include "helpFunctions.h"
+#include "instructions.h"
+
+
+
 
 int main() {
+
+
+
     printf("Hello, World!\n");
     return 0;
+
+
 }
+
+
+void evalInstruction(char * input, char ** pInput){ //tager 2 pointere som input
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
