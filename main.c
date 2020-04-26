78795573a92e1ce3613f41f9a789d3cf94546ec8
peterrevsbech <peter.revsbech@gmail.com>
2020-04-26 13:50:55 +0200

    testprogram

--- main.c
+++ main.c
@@ -4,21 +4,28 @@
 
 
 
+void evalTest();
 
 int main() {
-
-
-
     printf("Hello, World!\n");
+
+    char output[100];
+    char eksempel[] = "hejmaltehej";
+    evalTest(eksempel, output);
+
+
+
+    printf("%s",output);
     return 0;
-
-
 }
 
 
-void evalInstruction(char * input, char ** pInput){ //tager 2 pointere som input
-
-
+void evalTest(char * input, char * output){
+    for (int i =0; i<11;i++){
+        output[i] = input[i] - 32;
+    }
+    output[11]='\0';
+}
 
 
 
